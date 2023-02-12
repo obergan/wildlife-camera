19020c3dc35f04e37f35d89c7c38703801220973
Andreas Oberg <andreas.oberg@sick.se>
2023-02-12 12:44:52 +0100

    Visual fixups

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from sqlalchemy import desc, extract, distinct
 from sqlalchemy.types import DateTime
 from . import db, IMAGE_FOLDER
@@ -19,7 +19,8 @@ class Image(db.Model):
                             unique=True,
                             nullable=False)
 
-def get_image_info(images):
+# image info = (path, date-time string)
+def get_image_info(images)->List[Tuple[str, str]]:     
     return [(image.file_name, image.time_stored.strftime("%Y-%m-%d %H:%M:%S")) for image in images]
 
 
@@ -70,9 +71,7 @@ def get_images():
 
 def update_database():
     
-    print(db)
     image_files = get_images()
-    print(image_files)
     for file in image_files:
         # Check if the file is already in the database
         image = db.session.query(Image.file_name).filter_by(file_name=file).first()
@@ -86,6 +85,7 @@ def update_database():
 
     for db_im in images_in_db:
         if db_im not in image_files:
+            # if db entry does not exist in directory, delete it
            db.session.query(Image).filter(Image.file_name == db_im).delete()
            db.session.commit()
 
@@ -93,19 +93,30 @@ def get_years():
     years = db.session.query(extract('year', Image.time_stored)).distinct().all()
     return [year[0] for year in years]
 
-def get_images_from_year(year):
+def get_images_from_year(year)->Dict[str, Tuple[str, str]]:
     image_dict = {}
+    # Ectract every image-filled month
     months = db.session.query(extract('month', Image.time_stored)).filter(extract('year', Image.time_stored) == year).distinct().all()
     months = [month[0] for month in months]
-    print("Tjen")
-    print(months)
+
+    # Example February : [(path1, date1), (path2, date2)]
     for month in months:
         images_in_month = db.session.query(Image).filter(extract('year', Image.time_stored) == year,
                                                          extract('month', Image.time_stored) == month).all()
         image_info = get_image_info(images_in_month)
         print(image_info)
-        image_dict[str(month)] = image_info
+        image_dict[month_from_number(month)] = image_info
 
     return image_dict
+
+def month_from_number(month_number):
+    month_names = ["January", "February", "March", "April", "May", "June",
+                   "July", "August", "September", "October", "November", "December"]
+    if month_number >= 0 and month_number <= 12:
+        return month_names[month_number-1]
+    else:
+        return "Invalid month number"
+
+
 
 
